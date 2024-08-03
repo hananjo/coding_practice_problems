b2d75d2218363a1d39204fb0320a4661e16bd168
Hanan <hanan91jomaa@gmail.com>
2024-08-02 18:46:25 -0700

    Added additional notes to Problem: 334

--- Arrays/increasing_triplet_subsequence.js
+++ Arrays/increasing_triplet_subsequence.js
@@ -1,4 +1,4 @@
-
+//334. Increasing Triplet Subsequence (Leetcode Medium)
 // Given an integer array nums, 
 // return true if there exists a triple of indices (i, j, k)
 //  such that i < j < k and nums[i] < nums[j] < nums[k]. 
