6ea3816cf5aee3ed391195dbe741e7be61b984f0
Hanan <hanan91jomaa@gmail.com>
2025-01-13 13:45:04 -0800

    Added additional naive soultion to Max Subarray solution

--- Arrays/maximum_subarray.js
+++ Arrays/maximum_subarray.js
@@ -30,3 +30,20 @@ var maxSubArray = function(arr) {
     }
     return res
 };
+
+//O(n) time complexity and 0(1) space complexity
+
+//Naive solution below is O(n^2) time complexity with O(1) space
+
+// function maxSubArraySum(arr) {
+//     let res = arr[0]
+//     for(let i = 0; i < arr.length; i++) {
+//         let currSum = 0
+
+//         for(let j = i; j < arr.length; j++) {
+//             currSum = currSum + arr[j]
+//             res = Math.max(res, currSum)
+//         }
+//     }
+//     return res
+// }
